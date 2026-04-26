f95b13c3a9aa324e6fc7333dbe142f2ed786017e
mohnishkumar <kumarmohnish77@gmail.com>
2026-04-26 14:01:21 +0530

    url chnages

--- lib/app/core/config/app_config.dart
+++ lib/app/core/config/app_config.dart
@@ -2,7 +2,7 @@ class AppConfig {
   static const String appName = 'UrgentHai';
 
   // API
-  static const String baseUrl = 'http://urgenhai-001-site1.stempurl.com/api/';
+  static const String baseUrl = 'http://api.urgent-hai.com/api/';
 
   // Hive Boxes
   static const String authBox = 'authBox';
